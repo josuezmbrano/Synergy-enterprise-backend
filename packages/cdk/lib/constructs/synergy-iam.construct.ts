c2f4acd923cf924fbb75e6f338922c25a3a33cb8
Josue <josuezambrano2104@gmail.com>
2026-09-01 08:00:01 -0400

    fix(cdk): grant lambda role read permissions for secrets manager complete arn

--- packages/cdk/lib/constructs/synergy-iam.construct.ts
+++ packages/cdk/lib/constructs/synergy-iam.construct.ts
@@ -5,18 +5,15 @@ import { Construct } from "constructs";
 
 
 export interface SynergyIamConstructProps {
-    secretNamePattern?: string
+    secretArn: string
 }
 
 export class SynergyIamConstruct extends Construct {
 
     public readonly role: iam.Role
 
-    constructor(scope: Construct, id: string, props?: SynergyIamConstructProps) {
+    constructor(scope: Construct, id: string, props: SynergyIamConstructProps) {
         super(scope, id)
-
-        const stack = cdk.Stack.of(this)
-        const secretPattern = props?.secretNamePattern ?? 'prod/synergy/*'
 
         // CREATE IDENTITY FOR LAMBDA
         this.role = new iam.Role(this, 'ExecutionRole', {
@@ -29,16 +26,13 @@ export class SynergyIamConstruct extends Construct {
             iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')
         )
 
-        // SECRETS MANAGER SECRETS DIRECTION DEFINITION
-        const prodSecretArn = `arn:aws:secretsmanager:${stack.region}:${stack.account}:secret:${secretPattern}`
-
         // SECRET REFERENCE CREATION
-        const importedSecrets = secretsmanager.Secret.fromSecretPartialArn(
-            this, 'ImportedProdSecrets', `${prodSecretArn}-??????`
+        const importedSecrets = secretsmanager.Secret.fromSecretAttributes(
+            this, 'ImportedProdSecrets', { secretCompleteArn: props.secretArn }
         )
 
         // READ ONLY PERMISSION ASSIGMENT TO LAMBDA ROLE
         importedSecrets.grantRead(this.role)
-        
+
     }
 }
